18ff8df870fe322958f7e0d81c3a4589b48f2a69
xichuanwushao <avatarwx@163.com>
2020-02-29 22:21:34 +0800

    注册与发现gateway

---
+++ chapter0103/src/main/java/org/xichuan/cloud/gateway/cloud/eurekaserver/Chapter0101Application.java
@@ -0,0 +1,13 @@
+package org.xichuan.cloud.gateway.cloud.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter0101Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter0101Application.class, args);
+    }
+
+}
